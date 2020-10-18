97bfa73c9b0f705c737240fa6635bd47bbd38be0
RahmaSoui <rahma.souissi.8@gmail.com>
2020-10-18 13:16:21 +0100

    TP1_Android

--- app/src/main/java/com/example/tp1_android/MainActivity.java
+++ app/src/main/java/com/example/tp1_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tp1_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Question1", "activity_main.xml");
+        Log.d("Question2", "app_name");
+        Log.d("Question3", "AVD Manager");
+        Log.d("Question4", "Info");
+        Log.i("Etudiante", "Rahma Souissi 3 GII_RCI");
 
     }
 }
